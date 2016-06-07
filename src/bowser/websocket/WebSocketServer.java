99b025b7e37f0a8f13af59426333a70cbaed6189
Jason Mirra <mirraj2@gmail.com>
2016-06-07 14:33:50 -0700

    ssl websocket server

--- src/bowser/websocket/WebSocketServer.java
+++ src/bowser/websocket/WebSocketServer.java
@@ -4,16 +4,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
+import javax.net.ssl.SSLServerSocketFactory;
 
 public class WebSocketServer {
 
   public final int port;
+  private boolean ssl;
   private ServerSocket server;
   private Consumer<ClientSocket> onOpen = socket -> {
   };
 
   public WebSocketServer(int port) {
     this.port = port;
+  }
+
+  public WebSocketServer ssl() {
+    this.ssl = true;
+    return this;
   }
 
   public WebSocketServer onOpen(Consumer<ClientSocket> onOpen) {
@@ -23,7 +30,12 @@ public class WebSocketServer {
 
   public WebSocketServer start() {
     try {
-      server = new ServerSocket(port);
+      if (ssl) {
+        SSLServerSocketFactory sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        server = sslserversocketfactory.createServerSocket(port);
+      } else {
+        server = new ServerSocket(port);
+      }
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
