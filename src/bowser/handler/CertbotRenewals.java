b34e7f5180ed746b14f5d4c6fce6bb691682259e
Jason Mirra <mirraj2@gmail.com>
2020-02-23 15:34:25 -0600

    should work

--- src/bowser/handler/CertbotRenewals.java
+++ src/bowser/handler/CertbotRenewals.java
@@ -1,29 +1,35 @@
 package bowser.handler;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import java.io.File;
 
 import bowser.Controller;
 import bowser.Handler;
+import ox.IO;
 import ox.Log;
 import ox.OS;
 
 public class CertbotRenewals extends Controller {
+
+  private static final File appDir = OS.getAppFolder("ender");
 
   @Override
   public void init() {
     route("GET", "/.well-known/acme-challenge/*").to(handler);
   }
 
+  // certbot certonly --webroot -w /root/.ender -d files.ender.com
   private final Handler handler = (request, response) -> {
     String key = request.getSegment(2);
     Log.debug("Got certbot request: " + key);
 
-    File dir = new File(OS.getAppFolder("ender"), "certbot");
-    for (File child : dir.listFiles()) {
-      Log.debug(child);
-    }
+    checkState(!key.contains("/") && !key.contains("."));
 
-    Log.debug("done.");
+    File file = new File(appDir, ".well-known/acme-challenge/" + key);
+    checkState(file.exists());
+
+    IO.from(file).to(response.getOutputStream());
   };
 
 }
